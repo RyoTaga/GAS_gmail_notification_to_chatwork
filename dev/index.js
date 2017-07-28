9e1d1ff87a896ed6d7bcb32115040dfd30c171ce
RyoTaga <taga@n-di.co.jp>
2017-07-28 17:57:59 +0900

    add notification script

--- dev/index.js
+++ dev/index.js
@@ -1,2 +1,25 @@
-const hello = 'hello';
-console.log(hello);
+const sendNotification = () => {
+  const token = 'xxxxxxxxxxxxxxxxxxxxxxx';
+  const roomId = '123456789';
+  const label = 'label:xxx-xxxx-xx';
+  const threads = GmailApp.search(`${label} is:unread`);
+
+  if (threads == null || threads.lenght === 0) {
+    return;
+  }
+  threads.forEach((thread) => {
+    const msgs = thread.getMessages();
+    msgs.forEach((msg) => {
+      const subject = msg.getSubject();
+      const client = ChatWorkClient.factory({
+        token,
+      });
+      client.sendMessage({
+        room_id: roomId,
+        body: `[info][title]タイトル - ${subject}[/title]
+        ${body} [/info]`,
+      });
+      msg.markRead();
+    });
+  });
+};
